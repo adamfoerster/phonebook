d573a4b950cb9e7acbe9bf9ed2dbcbfdcab4177e
Adam Foerster <adam.foerster@gmail.com>
2017-08-20 18:18:26 -0300

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,17 @@
-var CACHE = 'cache-only';
+var CACHE = 'cache-and-update';
 
 self.addEventListener('install', function(evt) {
 	console.log('The service worker is being installed.');
+
 	evt.waitUntil(precache());
 });
 
+
 self.addEventListener('fetch', function(evt) {
 	console.log('The service worker is serving the asset.');
+
 	evt.respondWith(fromCache(evt.request));
+	evt.waitUntil(update(evt.request));
 });
 
 function precache() {
@@ -22,6 +26,14 @@ function fromCache(request) {
 	return caches.open(CACHE).then(function(cache) {
 		return cache.match(request).then(function(matching) {
 			return matching || Promise.reject('no-match');
+		});
+	});
+}
+
+function update(request) {
+	return caches.open(CACHE).then(function(cache) {
+		return fetch(request).then(function(response) {
+			return cache.put(request, response);
 		});
 	});
 }
