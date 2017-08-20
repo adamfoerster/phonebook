6a575c1b9a1d8d7a01089a7119505c0ca610598f
Adam Foerster <adam.foerster@gmail.com>
2017-08-20 18:28:24 -0300

    fix

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,8 @@ self.addEventListener('fetch', function(evt) {
 function precache() {
 	return caches.open(CACHE).then(function(cache) {
 		return cache.addAll([
-			'./img'
+			'./img',
+            './index.html'
 		]);
 	});
 }
