976242b38a704d9cd5c6f38290403519ab120e58
Adam Foerster <adam.foerster@gmail.com>
2017-08-20 17:31:40 -0300

    fix

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
-var backend = "http://phonebook.adamfoerster.com/backend/index.php?r=";
+if ('serviceWorker' in navigator) {
+	console.log('CLIENT: service worker registration in progress.');
+	navigator.serviceWorker.register('https://cdn.css-tricks.com/service-worker.js').then(function() {
+		console.log('CLIENT: service worker registration complete.');
+	}, function() {
+		console.log('CLIENT: service worker registration failure.');
+	});
+} else {
+	console.log('CLIENT: service worker is not supported.');
+}
+
+var backend = "https://phonebook.adamfoerster.com/backend/index.php?r=";
 
 var list = [];
 $('#table').hide();
 $('#edit').hide();
 $('#view').hide();
 
-var fetchTable = function (){
-    $.get(backend + "phonebook.list&t="+(new Date().getTime()),
-        function(data) {
-            list = [];
-            for (var person in data) {
-                if (data.hasOwnProperty(person)) {
-                    list.push(data[person]);
-                }
-            }
-            seeTable();
-    	})
-    	.fail(function(error) {
-    		console.log(error);
-    	});
+var fetchTable = function() {
+	$.get(backend + "phonebook.list&t=" + (new Date().getTime()),
+			function(data) {
+				list = [];
+				for (var person in data) {
+					if (data.hasOwnProperty(person)) {
+						list.push(data[person]);
+					}
+				}
+				seeTable();
+			})
+		.fail(function(error) {
+			console.log(error);
+		});
 }
 fetchTable();
 
 var seeTable = function() {
-    $('#tbody').html('');
-    $(list).each(function() {
-        $('#tbody').append(`<tr id="person_${this.id}"><td>${this.name}</td><td>${this.phone}</td></tr>`);
-    });
-    $('tr').click(function(e) {
-    	load(e.currentTarget.id);
-    });
-    $('#add').click(function(){
-        edit();
-    });
+	$('#tbody').html('');
+	$(list).each(function() {
+		$('#tbody').append(`<tr id="person_${this.id}"><td>${this.name}</td><td>${this.phone}</td></tr>`);
+	});
+	$('tr').click(function(e) {
+		load(e.currentTarget.id);
+	});
+	$('#add').click(function() {
+		edit();
+	});
 	$('#table').show();
 	$('#edit').hide();
 	$('#view').hide();
 };
 
 var save = function() {
-    let post = {id: $('#id').val(), name: $('#name').val(), phone: $('#phone').val()};
-    $.post(backend+'phonebook.save' , post)
-    .done(function(){
-        console.log('post sent');
-        fetchTable();
-    })
-    .fail(function(error) {
-		console.log(error);
-	});
+	let post = {
+		id: $('#id').val(),
+		name: $('#name').val(),
+		phone: $('#phone').val()
+	};
+	$.post(backend + 'phonebook.save', post)
+		.done(function() {
+			console.log('post sent');
+			fetchTable();
+		})
+		.fail(function(error) {
+			console.log(error);
+		});
 };
 
 var load = function(personId) {
-    var p = findPerson(personId.substring(7));
-    $('#view .card-body h1').html(p.name);
-    $('#view .card-body p').html(p.phone);
-    $('#editBtn').click(function() {
-        edit(p.id);
-    });
+	var p = findPerson(personId.substring(7));
+	$('#view .card-body h1').html(p.name);
+	$('#view .card-body p').html(p.phone);
+	$('#editBtn').click(function() {
+		edit(p.id);
+	});
 
 	$('#table').hide();
 	$('#view').show();
@@ -66,29 +81,29 @@ var load = function(personId) {
 	});
 };
 
-var edit = function(id=null) {
-    if (id){
-        var p = findPerson(id);
-        $('#id').val(id);
-        $('#name').val(p.name);
-        $('#phone').val(p.phone);
+var edit = function(id = null) {
+	if (id) {
+		var p = findPerson(id);
+		$('#id').val(id);
+		$('#name').val(p.name);
+		$('#phone').val(p.phone);
 
-    }
+	}
 	$('#save').click(save);
-    $('#table').hide();
+	$('#table').hide();
 	$('#view').hide();
 	$('#edit').show();
 };
 
 var exclude = function(id) {
 	// console.log('exclude', id);
-    $.get(backend+'phonebook.delete&id='+id)
-    .done(function(data){
-        fetchTable();
-    })
-    .fail(function(error) {
-		console.log(error);
-	});
+	$.get(backend + 'phonebook.delete&id=' + id)
+		.done(function(data) {
+			fetchTable();
+		})
+		.fail(function(error) {
+			console.log(error);
+		});
 };
 
 var findPerson = function(id) {
